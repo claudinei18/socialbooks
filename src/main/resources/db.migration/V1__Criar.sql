2ba6509131bf16d733d7abb03c5c9854263023df
Claudinei <claudineigom18@gmail.com>
2017-08-09 11:45:40 -0300

    Update

---
+++ src/main/resources/db.migration/V1__Criar.sql
@@ -0,0 +1,30 @@
+CREATE SEQUENCE livros_id_seq START 1 INCREMENT 1;
+
+CREATE SEQUENCE comentarios_id_seq START 1 INCREMENT 1;
+
+CREATE SEQUENCE autor_id_seq START 1 INCREMENT 1;
+
+CREATE TABLE autor(
+   id INTEGER PRIMARY KEY ,
+   nome text,
+   nascimento date,
+   nacionalidade text
+);
+
+CREATE TABLE livro(
+   id INTEGER PRIMARY KEY ,
+   nome text,
+   publicacao date,
+   editora text,
+   resumo text,
+   autor text,
+   AUTOR_ID integer not null references autor(id)
+);
+
+CREATE TABLE comentario(
+   id INTEGER PRIMARY KEY ,
+   texto text,
+   usuario text,
+   data timestamp,
+   LIVRO_ID integer not null references livro(id)
+);
